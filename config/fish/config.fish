347d0f4fa7b984033ee326f5ce5bc2bbe83e2b6d
Pavel Nichita @ work desktop <nichitapavel@live.com>
2020-09-25 12:58:44 +0100

    -Fix ANDROID_SDK_ROOT/ANDROID_HOME for local sessions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,9 +2,14 @@
 # Keeping it for history reasons
 #. ~/.config/fish/fish_aliases
 
+# https://fishshell.com/docs/current/cmds/status.html
+# status is-interactive: returns 0 if fish is interactive - that is, connected to a keyboard.
+# status is-login: returns 0 if fish is a login shell - that is, if fish should perform login tasks such as setting up the PATH
+# still don't know what's the real difference, physically with the pc "is-interactive" seems to work better.
+
 # These cmd can be replaced with fish functions
 # Why "--is-interactive" -> don't really know
-if status --is-interactive
+if status is-interactive
   abbr --add ll "ls -hl --group-directories --color=auto"
   abbr --add la "ls -hlA --group-directories --color=auto"
   abbr --add update-dist "sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt-get autoremove -y && sudo apt-get autoclean"
@@ -12,14 +17,12 @@ end
 
 # Add ~/.local/bin to PATH
 # Why "--is-login" -> don't really know
-if status --is-login
+if status is-login
   set -x PATH ~/.local/bin $PATH
 end
 
-# TODO there seems to be a problem with desktop session
-# at my work pc, maybe it should be --is-interactive?
 # Set ANDROID_SDK_ROOT environment variables
-if status --is-login
+if status is-interactive
   # is it empty?                 does it exist?
   if set -q -- $ANDROID_SDK_ROOT; or set -q ANDROID_SDK_ROOT
     set -x ANDROID_SDK_ROOT /opt/android
