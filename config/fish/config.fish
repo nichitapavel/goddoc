8251d52365ba8b2f47074e81c3d0ad32a2336cec
Pavel Nichita @ quantum1net <pavel.nichita@quantum1net.com>
2021-03-11 11:19:36 +0000

    -Add full-upgrade to fish abbreviations too

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,7 +17,7 @@
 if status is-interactive
   abbr --add ll "ls -hl --group-directories --color=auto"
   abbr --add la "ls -hlA --group-directories --color=auto"
-  abbr --add update-dist "sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt-get autoremove -y && sudo apt-get autoclean"
+  abbr --add update-dist "sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt-get autoremove -y && sudo apt-get autoclean"
 end
 
 # Add ~/.local/bin and ~/bin to PATH
