1cb422fbc4f545c8901f29dd0020faee749af3da
Pavel@IAC <pavel.nichita@iac.es>
2023-11-30 12:31:16 +0000

    Shortcut to search through the goddoc

---
+++ config/fish/functions/ggrep.fish
@@ -0,0 +1,8 @@
+# Defined in - @ line 1
+# To specify a list of hosts where to copy files do
+#   pscp -h <hosts.txt> <file_to_copy> <path_where_to_copy_on_remote>
+# For more info check http://manpages.ubuntu.com/manpages/focal/man1/parallel-scp.1.html
+function ggrep --wraps=grep --description 'alias ggrep=grep -iR <something> ~/git/goddoc/* '
+  grep -iR $argv ~/git/goddoc/*;
+end
+
