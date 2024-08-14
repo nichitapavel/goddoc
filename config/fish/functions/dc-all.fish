c8606eb9d5813e08f7aeaa67d07cc9d582a8acbc
Pavel@IAC <pavel.nichita@iac.es>
2024-08-14 11:28:42 +0100

    Change the terminal output of dc-all

--- config/fish/functions/dc-all.fish
+++ config/fish/functions/dc-all.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function dc-all --description 'alias dc-all=docker compose down && docker compose build && docker compose create && docker compose start'
-  docker compose down && docker compose build && docker compose create && docker compose start;
+  docker compose down && BUILDKIT_PROGRESS=plain docker compose build && docker compose create && docker compose start;
 end
