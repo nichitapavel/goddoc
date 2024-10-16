e4c339c10a5d2e83b1c7bad7a1011d5d52412498
Pavel@IAC <pavel.nichita@iac.es>
2024-10-16 11:26:34 +0100

    Exclude from ggrep some paths and files that most probably do not need to be checked

--- config/fish/functions/ggrep.fish
+++ config/fish/functions/ggrep.fish
@@ -3,6 +3,6 @@
 #   pscp -h <hosts.txt> <file_to_copy> <path_where_to_copy_on_remote>
 # For more info check http://manpages.ubuntu.com/manpages/focal/man1/parallel-scp.1.html
 function ggrep --wraps=grep --description 'alias ggrep=grep -iR <something> ~/git/goddoc/* '
-  grep -iR $argv ~/git/goddoc/*;
+  grep -iR --exclude="signature*.html" --exclude='LICENSE' --exclude-dir="sources.list.d" $argv ~/git/goddoc/*;
 end
 
